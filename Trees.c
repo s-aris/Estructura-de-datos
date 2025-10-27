a1a7b36ca2faca57ecece1eb30df01c715ed5641
s-aris <sara.hernandezca@udlap.mx>
2025-10-27 10:44:32 -0600

    Update Trees.c

--- Trees.c
+++ Trees.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 
-int main() {
-    int treeNodes[] = {50, 30, 90, 10, 40, 70, 100};
+#define MAXSIZE 15  
 
-    printf("Mapping of tree as an array:\n");
-    for (int i = 0; i < 7; i++) {
-        printf("Index %d → %d\n", i, treeNodes[i]);
+int main() {
+    //1.Llenar el arbol
+    int treeNodes[MAXSIZE] = {
+        10,   
+        50,   
+        90,   
+        88,   
+        100,  
+        78,   
+        0,   
+        99,   
+        7,    
+        89,   
+        20,   
+        67,   
+        76,   
+        -1,   
+        -1    
+    };
+
+    // 2)Imprimir el arbol
+    printf("Mapping of the binary tree as an array:\n\n");
+    for (int i = 0; i < MAXSIZE; i++) {
+        printf("Index %2d → %3d\n", i, treeNodes[i]);
     }
 
     return 0;
