f20db2f6022caef0d9f462255fe1ad3fc168104d
s-aris <sara.hernandezca@udlap.mx>
2025-10-28 22:27:10 -0600

    Create BinaryTree.c

---
+++ BinaryTree.c
@@ -0,0 +1,4 @@
+/*/Inserts a sequence of integers into a Max-Heap stored in an array - void insert(int item)
+Prints the heap after each insertion - void showHeap(void)
+Removes the maximum element (the root) K times, reheapifies downward each time, and prints the heap after each removal - int removeMax(void) & void reHeapDown(int i)
+Provides a function to print each node with its left/right children (using indices) to reinforce the array-as-tree mapping. ???/*/
